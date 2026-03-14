08b49edfc90838fb388488fce758c5e0ff8a2347
leo <kookyleo@gmail.com>
2026-03-15 04:48:06 +0800

    refactor: unify React Native package naming
    - Rename from 'react-native-graphviz' to 'graphviz-anywhere-react-native'
    - Align with web (graphviz-anywhere-web) and rust (graphviz-anywhere)
    - Update error messages and podspec references
    - Rename codegenConfig spec name for consistency

--- react-native/src/index.ts
+++ react-native/src/index.ts
@@ -1,7 +1,7 @@
 import { NativeModules, Platform } from 'react-native';
 
 const LINKING_ERROR =
-  `The package 'react-native-graphviz' doesn't seem to be linked. Make sure:\n\n` +
+  `The package 'graphviz-anywhere-react-native' doesn't seem to be linked. Make sure:\n\n` +
   Platform.select({
     ios: '- You have run `pod install`\n',
     macos: '- You have run `pod install`\n',
